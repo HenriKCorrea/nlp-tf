0651e46980d38e00051ebf4501767e3b2a604979
Henrique Krausburg Correa <henrique.krausburg.correa@gmail.com>
2025-05-04 11:13:21 +0000

    feat: Add dataset download sample

---
+++ main.py
@@ -0,0 +1,7 @@
+import datasets
+
+retrieval_nl_code_dataset = datasets.load_dataset("NTU-NLP-sg/xCodeEval", "retrieval_nl_code", trust_remote_code=True)
+print(retrieval_nl_code_dataset)
+
+retrieval_corpus_code_dataset = datasets.load_dataset("NTU-NLP-sg/xCodeEval", "retrieval_corpus")
+print(retrieval_corpus_code_dataset)
