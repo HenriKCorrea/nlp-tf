3fbd61a4aa39faf6c24a9da13a7669ef3ba1fbc7
Henrique Krausburg Correa <henrique.krausburg.correa@gmail.com>
2025-05-30 19:12:21 -0300

    feat: Implement dataset loading for retrieval tasks

--- src/main.py
+++ src/main.py
@@ -3,5 +3,5 @@ import datasets
 retrieval_nl_code_dataset = datasets.load_dataset("NTU-NLP-sg/xCodeEval", "retrieval_nl_code", trust_remote_code=True)
 print(retrieval_nl_code_dataset)
 
-retrieval_corpus_code_dataset = datasets.load_dataset("NTU-NLP-sg/xCodeEval", "retrieval_corpus")
+retrieval_corpus_code_dataset = datasets.load_dataset("NTU-NLP-sg/xCodeEval", "retrieval_corpus", trust_remote_code=True)
 print(retrieval_corpus_code_dataset)
