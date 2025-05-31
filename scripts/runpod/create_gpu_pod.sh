151386eed95fcda5b2386e7d87473a3113c4b1d2
Henrique Krausburg Correa <henrique.krausburg.correa@gmail.com>
2025-05-30 23:27:37 -0300

    refactor: Add script to create a GPU pod on RunPod service

--- scripts/runpod/create_gpu_pod.sh
+++ scripts/runpod/create_gpu_pod.sh
@@ -3,7 +3,7 @@
 # Ensure gopass is installed and configured to retrieve the API key
 # Usage: ./create-pod.sh
 
-create_pod_api() {
+create_gpu_pod_api() {
 
     curl --request POST \
     --url https://rest.runpod.io/v1/pods \
@@ -79,5 +79,5 @@ create_pod_api() {
 # Check if the script is being run directly (not sourced)
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
   set -euo pipefail
-  create_pod_api "$@"
+  create_gpu_pod_api "$@"
 fi
