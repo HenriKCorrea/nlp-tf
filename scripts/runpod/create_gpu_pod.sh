dcb5c364c245f0c1da9644bbac6c1ca714e37bec
Henrique Krausburg Correa <henrique.krausburg.correa@gmail.com>
2025-07-08 00:17:31 -0300

    chore: update setup scripts

--- scripts/runpod/create_gpu_pod.sh
+++ scripts/runpod/create_gpu_pod.sh
@@ -3,68 +3,29 @@
 # Ensure gopass is installed and configured to retrieve the API key
 # See https://docs.runpod.io/api-reference/pods/POST/pods
 
+# cloudType: SECURE | COMMUNITY
+# gpuTypeIds: [ "NVIDIA RTX A4000", "NVIDIA RTX 3090" ]
+
 _generate_payload_template() {
     cat <<EOF
 {
-    "allowedCudaVersions": [],
-    "cloudType": "COMMUNITY",
     "computeType": "GPU",
-    "containerDiskInGb": 300,
-    "countryCodes": [],
-    "dataCenterIds": [
-        "US-TX-4",
-        "US-IL-1",
-        "US-TX-3",
-        "US-GA-2",
-        "US-WA-1",
-        "US-TX-1",
-        "US-KS-3",
-        "EU-RO-1",
-        "US-CA-2",
-        "US-NC-1",
-        "US-DE-1",
-        "US-KS-2",
-        "CA-MTL-1",
-        "CA-MTL-3",
-        "CA-MTL-2",
-        "EU-SE-1",
-        "EU-CZ-1",
-        "EU-NL-1",
-        "EUR-IS-1",
-        "EUR-IS-2",
-        "EUR-IS-3",
-        "EUR-NO-1",
-        "OC-AU-1",
-        "AP-JP-1",
-        "EU-FR-1",
-        "US-GA-1"
-    ],
-    "dataCenterPriority": "availability",
-    "dockerEntrypoint": [],
-    "dockerStartCmd": [],
-    "env": {
-        "JUPYTER_PASSWORD": "{{ RUNPOD_SECRET_jupyter }}"
-    },
-    "globalNetworking": true,
-    "gpuCount": 1,
-    "gpuTypeIds": [
-        "NVIDIA A100 80GB PCIe"
-    ],
-    "gpuTypePriority": "custom",
-    "imageName": "runpod/pytorch:2.8.0-py3.11-cuda12.8.1-cudnn-devel-ubuntu22.04",
-    "interruptible": true,
-    "locked": false,
-    "minDiskBandwidthMBps": 123,
-    "minDownloadMbps": 123,
-    "minRAMPerGPU": 8,
-    "minUploadMbps": 123,
-    "minVCPUPerGPU": 2,
     "name": "dev-pod-gpu",
+    "imageName": "runpod/pytorch:2.8.0-py3.11-cuda12.8.1-cudnn-devel-ubuntu22.04",
     "ports": [
         "8888/http,22/tcp"
     ],
-    "supportPublicIp": true,
-    "vcpuCount": 2
+    "env": {
+        "JUPYTER_PASSWORD": "{{ RUNPOD_SECRET_jupyter }}"
+    },
+    "cloudType": "COMMUNITY",
+    "volumeInGb": 1,
+    "containerDiskInGb": 300,
+    "gpuCount": 5,
+    "gpuTypeIds": [
+        "NVIDIA RTX A4000"
+    ],
+    "interruptible": false
 }
 EOF
 }
@@ -109,7 +70,7 @@ create_gpu_pod_api() {
     request_cmd+=("--url" "https://rest.runpod.io/v1/pods")
     request_cmd+=("--header" "Authorization: Bearer $(gopass show -o cloud/runpod/api)")
     request_cmd+=("--header" "Content-Type: application/json")
-    payload="$(_generate_payload_template)"
+    payload="$(_generate_payload_template | _pick_network_volume_id)"
     request_cmd+=("--data" "$payload")
 
     # Execute the request
